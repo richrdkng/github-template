1d19297b7ab35104a121684394e0215874a66747
Richard King <richrdkng@gmail.com>
2019-10-13 16:48:31 +0200

    feat(cli): add working CLI - copy templates from src to dest

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
-console.log('hello there!', process.cwd())
+// get args -----------------------------------------------------------------------------------------------------------*
+const assert = require('assert')
+const [,, dir] = process.argv
+
+// simple cwd guard
+assert(dir === '.', `Current directory must be specified as ".", got: "${dir}"`)
+
+// prepare paths ------------------------------------------------------------------------------------------------------*
+const path = require('path')
+
+const pkg = require('../package.json')
+const cwd = process.cwd()
+const src = path.join(__dirname, '../.github')
+const dest = path.join(cwd, dir, '/.github')
+
+// copy files ---------------------------------------------------------------------------------------------------------*
+const fs = require('fs-extra')
+
+console.log(`${pkg.name} v${pkg.version} - copy templates:`)
+console.log(`from ${src}`)
+console.log(`to ${dest}`)
+
+fs.copySync(src, dest)
